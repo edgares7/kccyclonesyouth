86c6ce673a8c4e40b1728945d1548dbbf06eb311
Edgar Esparza <edgar.i.esparza@gmail.com>
2020-11-28 11:16:39 -0600

    added new logo and favicon, and updated site name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img class="inline" alt="Vue logo" src="../assets/logo.png" />
+    <img class="inline" alt="Vue logo" src="../assets/images/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
